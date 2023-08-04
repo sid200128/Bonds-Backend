97c7ef723246888cf87e2b58f982bbc09e09dbc4
Siddhant Singh <siddhantsingh@Siddhants-MacBook-Air.local>
2023-08-04 16:52:16 +0530

    first commit

---
+++ src/main/java/com/bonds/backend/BondsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bonds.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BondsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BondsBackendApplication.class, args);
+	}
+
+}
